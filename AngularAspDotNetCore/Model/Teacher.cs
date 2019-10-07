94e819f603cba65691f16c61a4a6de8766f88a76
jorge perez <jorgeperez00@hotmail.com>
2019-10-07 13:10:00 -0400

    added EF dependencies

---
+++ AngularAspDotNetCore/Model/Teacher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularAspDotNetCore.Model {
+    public class Teacher     {
+        public string Name { get; set; }
+        public IList<Student> Students { get; set; }
+    }
+}
