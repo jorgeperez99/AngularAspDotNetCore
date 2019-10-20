2197522c27f2ed0df2d6ff7f8ca521cec6146f10
jorge perez <jorgeperez00@hotmail.com>
2019-10-20 12:54:48 -0400

    fixed bug found when deploying

---
+++ AngularAspDotNetCore/ClientApp/src/app/Model/ConfigurationSetting.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ConfigurationSetting = /** @class */ (function () {
+    function ConfigurationSetting() {
+    }
+    return ConfigurationSetting;
+}());
+exports.ConfigurationSetting = ConfigurationSetting;
+//# sourceMappingURL=ConfigurationSetting.js.map
