a5da75bc4e2c0bc1f69bad4e3f225018c7e73be3
jorge perez <jorgeperez00@hotmail.com>
2019-11-01 15:20:48 -0400

    get environment from service

--- AngularAspDotNetCore/Controllers/ConfigurationController.cs
+++ AngularAspDotNetCore/Controllers/ConfigurationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AngularAspDotNetCore.Model;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -13,15 +14,17 @@ namespace AngularAspDotNetCore.Controllers
     [ApiController]
     public class ConfigurationController : ControllerBase  {
         private IConfiguration configuration;
+        private IHostingEnvironment env;
 
-        public ConfigurationController(IConfiguration config) {
+        public ConfigurationController(IConfiguration config, IHostingEnvironment env) {
             configuration = config;
+            this.env = env;
         }
 
         [HttpGet]
         public ActionResult<ConfigurationSetting> Get() {
             var cs = new ConfigurationSetting();
-            cs.ConfigurationType = configuration["environmentType"];
+            cs.ConfigurationType = env.EnvironmentName;
             return this.Ok(cs);
         }
 
